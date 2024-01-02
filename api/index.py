0c216816b914ce980528be012182cee10c5b8d76
Docfile <achachia2003@yahoo.fr>
2024-01-02 11:05:08 +0100

    update-code

--- api/index.py
+++ api/index.py
@@ -52,5 +52,5 @@ def dataOnchain():
         'Ok-Access-Key': 'fa318372-3362-4e1b-82ef-63dc2ad468c6'
     }
     response = requests.request("GET", url, headers=headers, data=payload)
-    return Response(response.to_json(orient="records"), mimetype='application/json')
+    return Response(response.text.to_json(orient="records"), mimetype='application/json')
    
